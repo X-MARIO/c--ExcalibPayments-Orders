541335e8402b3cc4e1adf13dd4b1db4eacd6b953
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2025-03-16 18:19:07 +0400

    create models

--- ExcalibPayments.Orders.Damain/Entities/BaseEntity.cs
+++ ExcalibPayments.Orders.Damain/Entities/BaseEntity.cs
@@ -3,6 +3,8 @@
 public class BaseEntity
 {
     public long Id { get; set; }
+    
     public DateTime CreatedAt { get; set; }
+    
     public bool IsActive { get; set; }
 }
---
+++ ExcalibPayments.Orders.Damain/Entities/CartItemEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace ExcalibPayments.Orders.Damain.Entities;
+
+public class CartItemEntity : BaseEntity
+{
+    public string Name { get; set; } = null!;
+    
+    public int Quantity { get; set; }
+    
+    public decimal Price { get; set; }
+
+    public CartEntity? Cart { get; set; }
+    
+    public long? CartId { get; set; }
+}
